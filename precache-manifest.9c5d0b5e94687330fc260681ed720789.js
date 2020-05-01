074bc277938b5ba45f37009e871e48b25d3fdcef
Vikhyat Prabhu <vikhyat.prabhu@gmail.com>
2020-05-02 02:50:38 +0530

    Updates

---
+++ precache-manifest.9c5d0b5e94687330fc260681ed720789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43c51cc2b6d8469e1c2498718d903fc",
+    "url": "/ludo-react/index.html"
+  },
+  {
+    "revision": "15782aea73949699583d",
+    "url": "/ludo-react/static/css/main.2a208a4f.chunk.css"
+  },
+  {
+    "revision": "3b5eeb3ff34d6d44bf36",
+    "url": "/ludo-react/static/js/2.d0a908be.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ludo-react/static/js/2.d0a908be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15782aea73949699583d",
+    "url": "/ludo-react/static/js/main.0e995f45.chunk.js"
+  },
+  {
+    "revision": "956c881a8bdf56436d92",
+    "url": "/ludo-react/static/js/runtime-main.4f330aa4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.9381b36513384b6a3e19493180657cff.js"
+  "/ludo-react/precache-manifest.9c5d0b5e94687330fc260681ed720789.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ludo-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
